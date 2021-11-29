db6911b34088ab50fa55d9d8e9a856f6d64f945e
Raymond <xzha897@aucklanduni.ac.nz>
2021-11-30 00:18:02 +1300

    Added font for entire web-app

--- raymondxyz/src/components/about_me/AboutMe.js
+++ raymondxyz/src/components/about_me/AboutMe.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { colors } from '../../styling/colors'
 
 function AboutMe({
 
 }) {
     return (
-        <h1> About Me Section </h1>
+        <div 
+          className="full_section"
+          style={{ backgroundColor: colors.grey }}
+        >
+            <div 
+              className="section_main_content"
+            >
+                <h1> About Me Section </h1>
+            </div>
+        </div>
     )
 }
 
--- raymondxyz/src/components/welcome/Welcome.js
+++ raymondxyz/src/components/welcome/Welcome.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import  { colors } from '../../styling/colors'
 import { useTrail, animated } from 'react-spring';
 
-class Welcome extends React.Component {
-    render() {
-        return (
-            <h1> Hi This Is Landing Component </h1>
-        )
-    }
+function Welcome({
+
+}) {
+    return (
+        <div 
+          className="full_section"
+          style={{ backgroundColor: colors.blue }}
+        >
+            <div className="section_main_content">
+                <h1> Hi This Is Landing Component </h1>
+            </div>
+        </div>
+    )
 }
 
 export default Welcome
