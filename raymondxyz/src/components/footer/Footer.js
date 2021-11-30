ce282c88107f8897a35c0209df7adf5a47015596
Raymond <xzha897@aucklanduni.ac.nz>
2021-11-30 23:34:32 +1300

    Completed first version of footer

--- raymondxyz/src/components/footer/Footer.js
+++ raymondxyz/src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaInstagram, FaReact, FaCss3Alt } from "react-icons/fa";
 import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
 import { colors } from "../../styling/colors";
 
@@ -11,27 +11,26 @@ function Footer({
           className="footer_bar"
           style={{ backgroundColor: colors.green }}
         >
-            <div className="footer_content">
-                <a
-                  href="https://www.linkedin.com/in/raymondxyz/"
-                >
-                    <FaLinkedin color={colors.white} size="30px"></FaLinkedin>
-                </a>
-                <a
-                  href="https://github.com/raymondxyz"
-                >
-                    <AiFillGithub color={colors.white} size="30px"></AiFillGithub>
-                </a>
-                <a
-                  href="https://www.instagram.com/raymond.xyz/"
-                >
-                    <FaInstagram color={colors.white} size ="30px"></FaInstagram>
-                </a>
-                <a
-                  href="mailto:raymondxyzhang@gmail.com"
-                >
-                    <AiOutlineMail color={colors.white} size="30px"></AiOutlineMail>
-                </a>
+            <ul className="footer_content_left">
+                <li className="footer_link">
+                    <a href="https://www.linkedin.com/in/raymondxyz/"><FaLinkedin color={colors.black} size="30px"></FaLinkedin></a>
+                </li>
+                <li className="footer_link">
+                    <a href="https://github.com/raymondxyz"><AiFillGithub color={colors.black} size="30px"></AiFillGithub></a>
+                </li>
+                <li className="footer_link">
+                    <a href="https://www.instagram.com/raymond.xyz/"><FaInstagram color={colors.black} size ="30px"></FaInstagram></a>
+                </li>
+                <li className="footer_link"> 
+                    <a href="mailto:raymondxyzhang@gmail.com"><AiOutlineMail color={colors.black} size="30px"></AiOutlineMail></a>
+                </li>
+            </ul>
+            <div className="footer_content_right">
+                <p>
+                    Made using <FaReact className="footer_icon" color={colors.black} size="25px"></FaReact>
+                    + <FaCss3Alt className="footer_icon" color={colors.black} size="25px"></FaCss3Alt>
+                    RZ {new Date().getFullYear()}
+                </p>
             </div>
         </div>
     )
