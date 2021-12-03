24fb5099bec1291919451a1f50736d962a8b7576
Raymond <xzha897@aucklanduni.ac.nz>
2021-12-03 18:56:29 +1300

    Set sections position to absolute and add constant bar array

--- raymondxyz/src/components/welcome/Welcome.js
+++ raymondxyz/src/components/welcome/Welcome.js
@@ -1,9 +1,19 @@
 import React from "react";
 import  { colors } from "../../styling/colors";
+import SpringBar from "./SpringBar";
 import { FaLinkedin, FaInstagram } from "react-icons/fa";
 import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
 import { useTrail, animated } from "react-spring";
 
+// Create array for trail animation
+const bars = [];
+for (let i = 0; i < 10; i++) {
+    bars.push(
+        <SpringBar barWidth="10vw" barColor={colors.blue} key={i}> </SpringBar>
+    );
+}
+
+// Function that is actually exported to be rendered:
 function Welcome({
 
 }) {
