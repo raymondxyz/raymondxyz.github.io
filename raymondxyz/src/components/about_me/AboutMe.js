b0029d756592b3accd9f7ef1466ac99bbd3327c5
Raymond <xzha897@aucklanduni.ac.nz>
2021-12-05 00:32:19 +1300

    Add fade animations on about sections

--- raymondxyz/src/components/about_me/AboutMe.js
+++ raymondxyz/src/components/about_me/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Fade from "react-reveal/Fade";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
 import photo from "./../../assets/profile_photo.jpg";
 import { colors } from "../../styling/colors";
 import NavArrowDown from "../miscellaneous/NavArrowDown";
@@ -18,33 +19,38 @@ function AboutMe({
           style={{ backgroundColor: colors.grey }}
           id={id}
         >
-          <div className="aboutme_left" style={{ backgroundColor: colors.brightblue }}>
-            <div style={{ marginLeft: "15vw", marginTop: "50px" }}>
-              <NavArrowUp scrollUp={scrollUp} tooltipUp={tooltipUp} coloring={colors.white}></NavArrowUp>
-            </div>
 
-            <div style={{ width: "40vw", height: "auto" }}>
-              <img
-                src={photo}
-                alt="Profile Photo"
-                className="profileImage"
-              >
-              </img>
-              <div className="left_about_content">
-                <h2 style={{ color: colors.white, textAlign: "center", marginBottom: "10px" }}> Hi There! </h2>
-                <h3
-                  style={{ color: colors.white, textAlign: "center" }}
-                > Information information information information information information information information 
-                  information information information information
-                </h3>
+            <div className="aboutme_left" style={{ backgroundColor: colors.brightblue }}>
+            <ScrollAnimation animateIn={"animate__fadeInLeft"} delay="200" duration="0.7">
+              <div style={{ marginLeft: "15vw", marginTop: "50px" }}>
+                <NavArrowUp scrollUp={scrollUp} tooltipUp={tooltipUp} coloring={colors.white}></NavArrowUp>
               </div>
+              <div style={{ width: "40vw", height: "auto" }}>
+                <img
+                  src={photo}
+                  alt="Profile Photo"
+                  className="profileImage"
+                >
+                </img>
+                <div className="left_about_content">
+                  <h2 style={{ color: colors.white, textAlign: "center", marginBottom: "10px" }}> Hi There! </h2>
+                  <h3
+                    style={{ color: colors.white, textAlign: "center" }}
+                  > Information information information information information information information information 
+                    information information information information
+                  </h3>
+                </div>
+              </div>
+
+              <div style={{ marginLeft: "15vw" }}>
+                <NavArrowDown scrollDown={scrollDown} tooltipDown={tooltipDown} coloring={colors.white}></NavArrowDown>
+              </div>
+              </ScrollAnimation>
             </div>
 
-            <div style={{ marginLeft: "15vw" }}>
-              <NavArrowDown scrollDown={scrollDown} tooltipDown={tooltipDown} coloring={colors.white}></NavArrowDown>
-            </div>
-          </div>
+
           <div className="aboutme_right" style={{ backgroundColor: colors.paleblue }}>
+          <ScrollAnimation animateIn={"animate__fadeInRight"} delay="200" duration="0.7">
             <div className="right_about_content">
               <h1 style={{ color: colors.white }}>  Section Heading </h1>
               <h2 style={{ color: colors.white }}> Information about the sections content which continues on for 
@@ -71,6 +77,7 @@ function AboutMe({
               </div>
               
             </div>
+            </ScrollAnimation>
           </div>
 
         </div>
