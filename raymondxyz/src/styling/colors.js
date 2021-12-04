5c9301f97c3ca0f35a4ece67657be8ad7941d6ad
Raymond <xzha897@aucklanduni.ac.nz>
2021-12-04 23:10:12 +1300

    Update color palette with more options

--- raymondxyz/src/styling/colors.js
+++ raymondxyz/src/styling/colors.js
@@ -1,7 +1,14 @@
 export const colors = {
-    blue: "#1661BE",
+    blue: "#0052CC",
+    lightblue: "#1963D1",
+    brightblue: "#2684FF",
+    paleblue: "#4C9AFF",
+    pastelblue: "#B3D4FF",
     white: "#FFFFFF",
     black: "#000000",
     grey: "#bbbbbb",
     green: "#A0E989",
+    slate: "#172B4D",
+    lightslate: "#344563",
+    sentinel: "#B3BAC5",
 }
