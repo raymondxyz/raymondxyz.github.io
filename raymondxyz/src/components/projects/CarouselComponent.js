f5cfd8c8c3399614d24c45f51e2932fe81a8380b
Raymond <xzha897@aucklanduni.ac.nz>
2021-12-21 21:54:00 +1300

    Fix carousel positioning and style

--- raymondxyz/src/components/projects/CarouselComponent.js
+++ raymondxyz/src/components/projects/CarouselComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
 import photo from "../../assets/profile_photo.jpg";
 import { colors } from "../../styling/colors"
 import {FaAngleLeft, FaAngleRight} from "react-icons/fa";
@@ -8,40 +8,46 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 function CarouselComponent() {
     return (
       <CarouselProvider
-        naturalSlideWidth={200}
-        naturalSlideHeight={200}
+        naturalSlideWidth={60}
+        naturalSlideHeight={80}
         totalSlides={3}
         infinite={true}
+        isPlaying={true}
+        interval={7000}
       >
         <div className="carousel_wrapper">
 
           <ButtonBack className="button_back"><FaAngleLeft size="40px" color={colors.blue}/></ButtonBack>
 
-          <Slider className="carousel_container">
+          <div className="carousel_center">
+            <Slider className="carousel_slider">
 
-            <Slide index={0}>
-              <div className="carousel_slide">
-                <h1 className="slide_title"> Developets </h1>
-                <p className="slide_info"> Information information information information information </p>
-                <img src={photo} className="slide_image"></img>
-              </div>
-            </Slide>
-            <Slide index={1}>
-              <div className="carousel_slide">
-                <h1 className="slide_title"> OCR Tool </h1>
-                <p className="slide_infoS"> Information information information information information </p>
-                <img src={photo} className="slide_image"></img>
-              </div>
-            </Slide>
-            <Slide index={2}>
-              <div className="carousel_slide">
-                <h1 className="slide_title"> Something Else </h1>
-                <p className="slide_info"> Information information information information information </p>
-                <img src={photo} className="slide_image"></img>
-              </div>
-            </Slide>
+              <Slide index={0}>
+                <div className="carousel_slide">
+                  <h1 className="slide_title"> Developets </h1>
+                  <p className="slide_info"> Information information information information information </p>
+                  <img src={photo} className="slide_image"></img>
+                </div>
+              </Slide>
+              <Slide index={1}>
+                <div className="carousel_slide">
+                  <h1 className="slide_title"> OCR Tool </h1>
+                  <p className="slide_info"> Information information information information information </p>
+                  <img src={photo} className="slide_image"></img>
+                </div>
+              </Slide>
+              <Slide index={2}>
+                <div className="carousel_slide">
+                  <h1 className="slide_title"> Something Else </h1>
+                  <p className="slide_info"> Information information information information information </p>
+                  <img src={photo} className="slide_image"></img>
+                </div>
+              </Slide>
 
-          </Slider>
+            </Slider>
+
+            <DotGroup className="carousel_dotgroup"></DotGroup>
+          </div>
 
           <ButtonNext className="button_next"><FaAngleRight size="40px" color={colors.blue}/></ButtonNext>
           
