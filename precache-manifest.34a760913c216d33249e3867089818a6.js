1c86ed219a633689b092ca3938df1dc3db10503b
Raymond <xzha897@aucklanduni.ac.nz>
2021-12-25 15:28:11 +1300

    Updates

---
+++ precache-manifest.34a760913c216d33249e3867089818a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4c027dcc1d4520ab17b51b96761fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "414c14551667b3175615",
+    "url": "/static/css/2.e2027c6a.chunk.css"
+  },
+  {
+    "revision": "0a3f8d37a8e8f4ff85d1",
+    "url": "/static/css/main.ae77b357.chunk.css"
+  },
+  {
+    "revision": "414c14551667b3175615",
+    "url": "/static/js/2.cc7d5d35.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.cc7d5d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3f8d37a8e8f4ff85d1",
+    "url": "/static/js/main.d286308b.chunk.js"
+  },
+  {
+    "revision": "c8190155e544ffccbd30",
+    "url": "/static/js/runtime-main.ce0af3d2.js"
+  },
+  {
+    "revision": "df33fee1c7b5c2046e0c79204da59110",
+    "url": "/static/media/black_video.df33fee1.mp4"
+  },
+  {
+    "revision": "049969fb32bb9036ead9eec2feab9f6a",
+    "url": "/static/media/collage.049969fb.png"
+  },
+  {
+    "revision": "d22c08799f688bb73e01f19bcf99391e",
+    "url": "/static/media/profile_photo.d22c0879.jpg"
+  }
+]);
